1d605faac8ed44acb798100451fb8a6fb02f4406
photonnn <wasjiri@gmail.com>
2022-08-13 15:46:23 +0200

    Add a todo list for futher development

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@ import './style.css';
 import './eventListeners';
 
 console.log("Hello World");
+
+
+/*
+    TODO:
+    - Configure dates properly
+    - Add a form for user input
+    - Style changes: Redo task squares
+    - Task Finish button
+    - Task description/edit system
+    - make priorites matter, border color?
+    - Add Local storage
+*/
